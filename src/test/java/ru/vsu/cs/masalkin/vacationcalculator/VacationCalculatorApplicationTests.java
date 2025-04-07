65d39d79ce4139c68dd54353a5f169007e01da48
nmasalkin <nmasalkin96@yandex.ru>
2025-04-07 17:54:27 +0300

    Initial commit

---
+++ src/test/java/ru/vsu/cs/masalkin/vacationcalculator/VacationCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.masalkin.vacationcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
